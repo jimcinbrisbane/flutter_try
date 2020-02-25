cc7faf70411edaff00af2ecc7ce91786902d6161
Jim Chi <jimcinbrisbane@pm.me>
2020-02-25 16:05:52 +1000

    add flat button and change of state of the dice when clicks importing dart:math for randomizing the button number

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
@@ -15,9 +15,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDie = 5;
+  var rightDie = 2;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    leftDie = 6;
+                  });
+                },
+                child: Image.asset("images/dice$leftDie.png"),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      rightDie = 4;
+                    });
+                  },
+                  child: Image.asset("images/dice$rightDie.png")),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
